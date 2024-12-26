2401c475206e78af097341a9df9a38e970197342
sonaldesai23 <sonaldesai2108@gmail.com>
2024-12-26 12:41:06 +0530

    created the file literals

---
+++ core_java/literals_cwh.java
@@ -0,0 +1,33 @@
+public class literals_cwh {
+    public static void main(String[] args){
+        byte age = 34;
+        int age2 = 56;
+        short age4 = 43;
+        long age3 = 7832L;
+        char ch = 'A';
+        float f1 = 3.3f;
+        double d1 = 3.45D;
+        boolean a = true;
+        System.out.println(age);
+    
+        String str = "Sonal";
+        System.out.println(str);
+    }
+}
+
+/*
+ Java Tutorial: Literals in Java
+Literals
+A constant value that can be assigned to the variable is called a literal.
+
+101 – Integer literal
+10.1f – float literal
+10.1 – double literal (default type for decimals)
+‘A’ – character literal
+true – Boolean literal
+“Harry” – String literal
+Keywords
+Words that are reserved and used by the Java compiler. They cannot be used as an Identifier.
+
+{You can visit docs.oracle.com for a comprehensive list}
+ */
