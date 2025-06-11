73b7e7d62eeec5f2c916529dbdf3d7c9e43e1773
sonaldesai23 <sonaldesai2108@gmail.com>
2025-06-11 09:12:10 +0530

    New topic Arrays

---
+++ core_java/basics/arrays/main.java
@@ -0,0 +1,24 @@
+package core_java.basics.arrays;
+// import java.util.*;
+
+
+public class main {
+    public static void main(String[] args) {
+             
+        //Array Syntax -
+        
+            // datatype[] var_name = new datatype[size];
+        
+        int[] rolln; //declaration
+        rolln =  new int[5]; //initialization
+
+        System.out.println(rolln[3]);
+
+        String[] arr =  new String[4];
+        System.out.println(arr[0]); // o/p- null
+
+        // String str = null; //can write like this
+        // int num =  null; //cant write, gives error.
+
+    }
+}
