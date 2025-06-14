42fa3719945299bd8e60ccff01dc1c6ff81baaeb
sonaldesai23 <sonaldesai2108@gmail.com>
2025-06-14 09:33:56 +0530

    new file

--- core_java/basics/problems/arraylist_pb/maxitem.java
+++ core_java/basics/problems/arraylist_pb/maxitem.java
@@ -4,11 +4,14 @@ public class maxitem {
     public static void main(String[] args) {
         int[] arr = {1, 3, 23, 9, 18};
         
-        System.out.println(max(arr, 0, 4));
+        System.out.println(maxRange(arr, 0, 4));
     }
     
     //assumming arr is not empty.
     static int max(int[] arr) {
+        if (arr.length == 0) {
+            return -1;
+        }
         int maxVal =arr[0];
         for(int i = 1; i< arr.length; i++) {
             if( arr[i] > maxVal) {
@@ -20,7 +23,15 @@ public class maxitem {
 
 
     //finding max in a particular range
-    static int max(int[] arr, int start, int end) {
+    static int maxRange(int[] arr, int start, int end) {
+        // work on edge cases here, like arr being null.
+        if (end>start) {
+            return -1;
+        }
+        if(arr == null) {
+            return -1;
+        }
+
         int maxVal =arr[start];
         for(int i = start; i< end; i++) {
             if( arr[i] > maxVal) {
--- core_java/basics/problems/arraylist_pb/swap.java
+++ core_java/basics/problems/arraylist_pb/swap.java
@@ -3,11 +3,26 @@ import java.util.Arrays;
 
 public class swap {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 23, 9, 18};
-        swap(arr, 1, 3);
-
+        int[] arr = {1, 3, 23, 9, 18,65};
+        // swap(arr, 1, 3);
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
+
+    //Reversing
+    static void reverse(int[] arr) {
+        int start = 0; 
+        int end = arr.length-1;
+
+        while(start < end) {
+            //swap
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+
     static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
