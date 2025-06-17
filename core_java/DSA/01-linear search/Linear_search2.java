187e57261ff8e673f40cdaf8f7420ef5af59da93
sonaldesai23 <sonaldesai2108@gmail.com>
2025-06-17 14:38:04 +0530

    new file

--- core_java/DSA/01-linear search/Linear_search2.java
+++ core_java/DSA/01-linear search/Linear_search2.java
@@ -1,5 +1,44 @@
 package core_java.DSA.01-linear search;
 
 public class Linear_search2 {
-    
+    public static void main(String[] args) {
+	    int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+	    int target = 19;
+	    boolean ans = linearSearch3(nums, target);
+        System.out.println(ans);
+    }
+
+    // search the target and return true or false
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return false;
+    }
+
+    // search the target and return the element
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return element;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return Integer.MAX_VALUE;
+    } 
 }
