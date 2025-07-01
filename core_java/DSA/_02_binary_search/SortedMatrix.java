bd513254b87a9538d5e852239797779f38454155
sonaldesai23 <sonaldesai2108@gmail.com>
2025-07-01 19:20:33 +0530

    Bubble_sort

---
+++ core_java/DSA/_02_binary_search/SortedMatrix.java
@@ -0,0 +1,24 @@
+package core_java.DSA._02_binary_search;
+
+public class SortedMatrix {
+    public static void main(String[] args)  {
+
+    }
+
+    static int[] binarysearch(int[] matrix, int row, int cstart, int cend, int target) {
+        while(cstart <= cend){
+            int mid = cstart + (cend - cstart) = 2;
+            if()
+        }
+    
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int col = matrix[0].length; // be cautioous, matrix may be empty
+        
+        if(rows == 1) {
+
+        }
+    }
+}
---
+++ core_java/DSA/_03_sorting/Bubble_sort/main.java
@@ -0,0 +1,37 @@
+package core_java.DSA._03_sorting.Bubble_sort;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        int[] arr = {-1, -34, 5, 4,0, -78, 3, 2, 1};
+        // int[] arr = {3, 15, 29, 11, 1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+        //run the steps n-1 times.
+        for(int i = 0; i < arr.length; i++){
+            swapped = false;
+            //for each step,max item will come at the last respective index.
+            for(int j = 1; j < arr.length - i; j++) {
+                
+                //swap the item that is smaller than the previous item.
+                if(arr[j] < arr[j-1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            //if u didnt swap for a partivular value of i, means arr is sorted,hence stop program.
+            if(!swapped){
+                // !false = true
+                break;
+            }
+        }
+    }
+}
