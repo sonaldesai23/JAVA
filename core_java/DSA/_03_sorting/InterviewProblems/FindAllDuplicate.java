8e31a590f6e47c5f0fcfa97556b4a9ca5c0c455e
sonaldesai23 <sonaldesai2108@gmail.com>
2025-07-07 11:00:19 +0530

    few changes

--- core_java/DSA/_03_sorting/InterviewProblems/FindAllDuplicate.java
+++ core_java/DSA/_03_sorting/InterviewProblems/FindAllDuplicate.java
@@ -1,13 +1,9 @@
 package core_java.DSA._03_sorting.InterviewProblems;
-// https://leetcode.com/problems/find-the-duplicate-number/?__cf_chl_tk=4DZo8pY8be5MyP5Ex0WW_Ml9Imt2k8UmlRmeqbivqGM-1751629744-1.0.1.1-VTVk5LRvTr9WgozidKC7WB1Atb5up81ksOXXmQxWzaM
 
-public class FindDuplicate {
-    public static void main(String[] args) {
-        int[] nums = {1,3,4,2,2};
-        int ans = findDuplicate(nums);
-        System.out.println(ans);
-    }
-    static int findDuplicate(int[] nums) {
+import java.util.List;
+
+public class FindAllDuplicate {
+    public List<Integer> findDuplicates(int[] nums) {
         int i = 0;
         while (i < nums.length) {
 
@@ -24,7 +20,6 @@ public class FindDuplicate {
         }
         return -1;
     }
-
     static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
