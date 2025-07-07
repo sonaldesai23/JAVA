407f10a698ddc659c30a668dcf5078698a3c1bce
sonaldesai23 <sonaldesai2108@gmail.com>
2025-07-07 11:13:41 +0530

    FindAllDuplicate

--- core_java/DSA/_03_sorting/InterviewProblems/FindAllDuplicate.java
+++ core_java/DSA/_03_sorting/InterviewProblems/FindAllDuplicate.java
@@ -1,24 +1,36 @@
 package core_java.DSA._03_sorting.InterviewProblems;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FindAllDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> ans = new FindAllDuplicate().findDuplicates(nums);
+        System.out.println(ans);
+    }
+
     public List<Integer> findDuplicates(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-
             if(nums[i] != i + 1) {
                 int correct = nums[i] - 1;
                 if (nums[i] != nums[correct]) {
                     swap(nums, i , correct);
                 } else {
-                    return nums[i];
+                    i++;
                 }
             } else {
                 i++;
             }
         }
-        return -1;
+        List<Integer> duplicates = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                duplicates.add(nums[j]);
+            }
+        }
+        return duplicates;
     }
     static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
@@ -27,3 +39,4 @@ public class FindAllDuplicate {
     }
     
 }
+ 
