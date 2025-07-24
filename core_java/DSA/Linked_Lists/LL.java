4ac8afca90e0a48e57307b1d961c85f9e4cf76f4
sonaldesai23 <sonaldesai2108@gmail.com>
2025-07-24 07:02:37 +0530

    LinkedList Problems

---
+++ core_java/DSA/Linked_Lists/LL.java
@@ -0,0 +1,27 @@
+package core_java.DSA.Linked_Lists;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+    public LL() {
+        this.size = 0;
+    }
+
+    
+    private int Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+     
+}
---
+++ core_java/DSA/Linked_Lists/Main.java
@@ -0,0 +1,7 @@
+package core_java.DSA.Linked_Lists;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list =  new LL();
+    }
+}
